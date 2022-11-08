a60459d7e492b5fd15ff7e8ce6e171f36366f2a9
azulkipli <azulkipli@gmail.com>
2022-11-08 22:45:23 +0700

    init nuxt 3 with tailwindcss

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+
+import { defineNuxtConfig } from 'nuxt/config'
+// import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  telemetry: false,
+  typescript: {
+    shim: false
+  },
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+  colorMode: {
+    preference: 'system', // default theme
+    dataValue: 'theme', // activate data-theme in <html> tag
+    classSuffix: '',
+  },
+})
+
+
