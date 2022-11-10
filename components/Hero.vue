41d636b4f8d4ceccdb8ffdff7695aa8c981f6356
azulkipli <azulkipli@gmail.com>
2022-11-10 13:02:22 +0700

    udpate topNav

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="hero min-h-screen bg-base-200">
         <div class="hero-content text-center">
-            <div class="max-w-md">
+            <div class="sm:max-w-sm md:max-w-md">
                 <h1 class="text-5xl font-bold">Halo NuxtJS 3</h1>
                 <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
                     exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
