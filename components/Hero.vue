0b935674f4776b62810912861c96f24f2ad86bd3
azulkipli <azulkipli@gmail.com>
2022-11-17 19:45:07 +0700

    english lipsum

--- components/Hero.vue
+++ components/Hero.vue
@@ -3,8 +3,10 @@
         <div class="hero-content text-center">
             <div class="sm:max-w-sm md:max-w-md">
                 <h1 class="text-5xl font-bold">Halo NuxtJS 3</h1>
-                <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
-                    exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                <p class="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+                    has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a
+                    galley of type and scrambled it to make a type specimen book.
+                </p>
                 <button aria-label="get-started" class="btn btn-accent">Get Started</button>
             </div>
         </div>
